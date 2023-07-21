853df6ff1d882b46c428ac3bcd992999882cdd59
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2023-07-21 15:49:55 +0200

    🐍 Python module with convenience imports.

---
+++ src/weboptout/__init__.py
@@ -0,0 +1,6 @@
+## Copyright © 2023, Alex J. Champandard.  Licensed under MIT; see LICENSE! ⚘
+
+__version__ = "dev"
+
+from .types import rsv, Reservation
+from .web import check_domain_reservation
