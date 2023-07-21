5b60e990bb17b617783214a7d859b41945906c4a
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2023-07-21 14:38:29 +0200

    🗃️ Basic objects and enums.

---
+++ src/weboptout/types.py
@@ -0,0 +1,50 @@
+## Copyright © 2023, Alex J. Champandard.  Licensed under MIT; see LICENSE! ⚘
+
+import enum
+
+
+class Status(enum.Enum):
+    """
+    Enumeration of status codes that can be used as return values to indicate
+    the result of an operation, or as the status code of a log record.
+    """
+
+    SUCCESS = 1
+    FAILURE = 0
+    ABORT = -1
+
+
+class Reservation:
+    """
+    A reservation of rights and all the information that goes with it, in
+    order to provide explanations.  The reservation can apply to any concept,
+    including a domain, a URL, a file.
+
+    This class is intended to be used via the `rsv` namespace, along with the
+    defaults YES, MAYBE, NO.  These can be cloned via the __call__ function to
+    create Reservations derived from that default.
+    """
+
+    def __init__(self, id: int, summary: str = None, records: list = None):
+        self._id = id
+        self.summary = summary
+        self.records = records or []
+
+    def __eq__(self, id: int):
+        return self._id == id
+
+    def __repr__(self):
+        return f'<weboptout.Reservation id={self._id} summary="{self.summary}">'
+
+    def __call__(self, /, summary: str, records: list = None):
+        return Reservation(self._id, summary, records)
+
+
+class rsv:
+    """
+    Namespace with default Reservations that can be cloned and used for comparison.
+    """
+
+    YES = Reservation(2)
+    MAYBE = Reservation(1)
+    NO = Reservation(0)
