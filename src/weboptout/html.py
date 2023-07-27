883d033a89195a0f7602807c05f67d3520e48a58
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2023-07-27 10:12:32 +0200

    💎 Now also checking for non-commercial reservations.

--- src/weboptout/html.py
+++ src/weboptout/html.py
@@ -5,28 +5,21 @@ import langdetect
 from bs4 import BeautifulSoup
 
 from .types import Status
-from .config import RE_TDM_CONCEPTS, RE_LEGAL_WORDS
+from .config import RE_TDM_CONCEPTS, RE_LEGAL_WORDS, RE_NFP_CONCEPTS
 
 
 __all__ = ["check_tos_reservation"]
 
 
-def check_tos_reservation(client, url: str, html: str) -> Status:
-    soup = BeautifulSoup(html, "html.parser")
-    text = "\n".join(_extract_paragraphs(soup))
-
-    para_count = text.count("\n") + 1
-
-    if len(text) > 1_000 and (lang := langdetect.detect(text)) != "en":
-        client.log(
-            Status.FAILURE,
-            f"Found a possible ToS page but language is '{lang.upper()}' at {url}.",
-        )
-        return Status.ABORT
-
+def _find_matching_paragraphs(patterns: list, text: str) -> list[tuple]:
+    """
+    For each paragraph in the text, apply all the patterns in order and stop when
+    there's a match.  The results are sorted by rank of the pattern to find which
+    is the most important.
+    """
     reasons = []
     for line in text.split("\n"):
-        for rank, regexp in enumerate(RE_TDM_CONCEPTS):
+        for rank, regexp in enumerate(patterns):
             match = regexp.search(line.rstrip("\n"))
             if not match:
                 continue
@@ -37,13 +30,29 @@ def check_tos_reservation(client, url: str, html: str) -> Status:
             while line[j] not in "().;" and j + 1 < len(line):
                 j += 1
 
-            if len(line[i : j + 1]) < 512:
-                explain = line[i : j + 1].lstrip("().,; ").rstrip("() ")
-                reasons.append((rank, explain, line))
+            explain = line[i : j + 1].lstrip("().,; ").rstrip("() ")
+            reasons.append((rank, explain, line))
             break
 
-    if len(reasons):
-        reasons = sorted(reasons, key=lambda x: x[0] * 1000 - len(x[1]))
+    return sorted(reasons, key=lambda x: x[0] * 1000 - len(x[1]))
+
+
+def check_tos_reservation(client, url: str, html: str) -> Status:
+    soup = BeautifulSoup(html, "html.parser")
+    text = "\n".join(_extract_paragraphs(soup))
+
+    para_count = text.count("\n") + 1
+
+    # Only english language is currently supported.
+    if len(text) > 1_000 and (lang := langdetect.detect(text)) != "en":
+        client.log(
+            Status.FAILURE,
+            f"Found a possible ToS page but language is '{lang.upper()}' at {url}",
+        )
+        return Status.ABORT
+
+    # Words that match data-mining concepts.
+    if len(reasons := _find_matching_paragraphs(RE_TDM_CONCEPTS, text)):
         client.log(
             Status.SUCCESS,
             f"Found a total of {len(reasons)} matching paragraphs out of "
@@ -53,29 +62,36 @@ def check_tos_reservation(client, url: str, html: str) -> Status:
         )
         return Status.SUCCESS
 
-    elif len(text) < 2_000:
-        size = len(text)
+    # Words that match not-for-profit reservations.
+    if len(reasons := _find_matching_paragraphs(RE_NFP_CONCEPTS, text)):
+        client.log(
+            Status.SUCCESS,
+            f"Found total of {len(reasons)} paragraphs matching "
+            f"non-commercial activities in Terms Of Service.",
+            highlight=reasons[0][1],
+        )
+        return Status.SUCCESS
+
+    if (size := len(text)) < 2_000:
         client.log(
             Status.FAILURE,
             f"Too little information extracted, only {size:,} bytes from "
-            f"the ToS page at {url}.",
+            f"the ToS page at {url}",
         )
 
         # Suggest fetching page again via HTTP with Selenium.
         return Status.RETRY
 
-    else:
-        legal_words = len(RE_LEGAL_WORDS.findall(text))
-        if legal_words < 36:
-            client.log(
-                Status.FAILURE,
-                f"The ToS page does not appear to contain a legal text at {url}.",
-            )
-            return Status.FAILURE
+    if len(RE_LEGAL_WORDS.findall(text)) < 36:
+        client.log(
+            Status.FAILURE,
+            f"The ToS page does not appear to contain a legal text at {url}",
+        )
+        return Status.FAILURE
 
     client.log(
         Status.FAILURE,
-        f"No direct matches found in {para_count} paragraphs found at {url}.",
+        f"No direct matches found in {para_count} paragraphs found at {url}",
     )
     return Status.ABORT
 
